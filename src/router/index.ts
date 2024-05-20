1ac77e810fe81e968969ea60f5d87bd8da0eb6ca
Miko O. Generale <mikognrl@gmail.com>
2024-05-20 14:24:02 +0800

    resolve conflict in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,10 +13,8 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
       component: AboutView
-    },
+    }
   ]
 })
 
